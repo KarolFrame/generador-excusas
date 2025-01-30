cb0e358b198cf43f25dd214cdb7622357919ea50
Carolina <97622225+KarolFrame@users.noreply.github.com>
2025-01-30 00:26:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+var who = [
+  "My dog",
+  "My sister",
+  "My grandma",
+  "My bird",
+  "My cousing",
+  "An alien",
+  "My neighbor",
+  "A ghost"
+];
+var action = [
+  "ate",
+  "peed",
+  "broke",
+  "cooked",
+  "set fire to",
+  "stole",
+  "hid",
+  "spilled hot chocolate on"
+];
+var what = [
+  "my homeworks",
+  "my laptop",
+  "the presentation",
+  "my code",
+  "my class project"
+];
+var when = [
+  "before the class",
+  "during breakfast",
+  "while I was sleeping",
+  "last night",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function printExcuse() {
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)] +
+    ".";
+
+  document.body.querySelector("#excuse").innerHTML = excuse;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  printExcuse();
 };
